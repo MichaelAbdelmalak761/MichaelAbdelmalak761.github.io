71e01441cba5772d766386b03308c665c33c2d22
MichaelAbdelmalak761 <michael.abdelmalak@jpschools.org>
2024-09-10 19:10:41 +0000

    added platforms and collectable to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,28 +38,31 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(0, 500, 200, 400); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(350, 500, 40, 400); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(520, 385, 30, 10);
+    createPlatform(670, 300, 20, 10);
+    createPlatform(1000, 450, 1000, 400);
+    createPlatform(780, 585, 100, 10)
+    createPlatform(520, 555, 30, 400)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
-    
+    createCollectable("max", 670, 100)
+    createCollectable("steve", 520,105)
+    createCollectable("database", 1200, 55)
+    createCollectable("grace", 520, 400)
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
-    
+    createCannon("bottom", 230, 1000)
+    createCannon("bottom", 430, 900)
+    createCannon("bottom", 560, 800)
+    createCannon("bottom", 860, 800)
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
